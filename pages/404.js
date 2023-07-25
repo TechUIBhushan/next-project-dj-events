4b1c631175ccd0893f1df925b65b4abde8f45ccf
Bhushan Sharma <bhushansharma@Bhushans-MacBook-Pro.local>
2023-07-26 01:06:15 +0530

    Added extra line

--- pages/404.js
+++ pages/404.js
@@ -10,7 +10,7 @@ export default function NotFoundPage() {
         <h1>
           <FaExclamationTriangle /> 404
         </h1>
-        <h4>Sorry, there is nothing here...</h4>
+        <h4>Sorry, there is nothing here....</h4>
         <Link href="/">Go Back Home</Link>
       </div>
     </Layout>
